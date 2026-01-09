3ddfb2eb94fdd29280edb6c3c5c97b51b7f16e87
Thubalethu Njapha <thubzthubz@gmail.com>
2026-01-09 15:23:44 +0200

    practicing coding habit

---
+++ python-basics/expressions/string_expressions.py
@@ -0,0 +1,39 @@
+print("hello" + "world")
+print("Thubalethu " + " Njapha")
+
+##numberical expressions
+
+print(5 + 2)
+print(5-2)
+print(5*2)
+print(5/2)
+print(5%2)
+print(10%2)
+
+
+
+print(5 + 2 * 10*(5/2) )
+
+#Logical operators
+
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
+
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
+print((not False) or False)
+print(False or (True and False))
+print(False or not (True and False))
+print((not True) and False or True)
+
+print((13 % 5) > 0)
+print("potato" == "potato")
+print("45" == 45)
+print(5>3 and 1 == 0)
+
+
---
+++ python-basics/expressions/variable.py
@@ -0,0 +1,23 @@
+x = 10
+y= 20
+my_name = "Thubalethu"
+
+print(x)
+print(y)
+print(my_name)
+
+#type of data
+#int,float
+print(20)
+print(100.50)
+
+#string
+
+str = "This is a string"
+ 
+print(str)
+
+#boolean 
+print(True)
+print(False)
+
