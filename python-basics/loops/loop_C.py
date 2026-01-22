7b4b7049f38109ae0437c52ba9658c09dc2cc720
Thubalethu Njapha <thubzthubz@gmail.com>
2026-01-22 15:05:04 +0200

    loop Practices

--- python-basics/loops/loop_C.py
+++ python-basics/loops/loop_C.py
@@ -90,3 +90,71 @@ def raise_power(base, exponent):
         result *= base
     return result
 
+#======================================================================================================
+# Write a function `censor_e(text)` that returns a new string where all 'e' characters
+# are replaced with '*'.
+
+# Example:
+#censor_e("speedy")  #-> 'sp**dy'
+#censor_e("pending") #-> 'p*nding'
+#censor_e("scene")   #-> 'sc*n*'
+#censor_e("heat")    #-> 'h*at'
+
+
+
+def censor_e(text):
+
+    for i in text:
+        if 'e' in text:
+            update_text =  text.replace('e','*')
+    
+    return update_text
+            
+
+print(censor_e("scene") ) 
+
+#=======================================================================================
+# Write a function `fizz_buzz(max_num)` that prints all numbers <= max_num
+# that are divisible by 3 or 5 but not both.
+# The function does not return a value, just prints.
+
+# Example:
+#fizz_buzz(18)
+# 3
+# 5
+# 6
+# 9
+# 10
+# 12
+# 18
+
+#fizz_buzz(33)
+# 3
+# 5
+# 6
+# 9
+# 10
+# 12
+# 18
+# 20
+# 21
+# 24
+# 25
+# 27
+# 33
+
+def fizz_buzz(max_num):
+
+    for i in range(1,max_num+1):
+        if (i % 3 == 0 or i % 5 == 0) and not (i % 3 == 0 and i % 5 == 0):
+
+            print(i)
+
+
+fizz_buzz(18)
+
+    
+
+
+
+
