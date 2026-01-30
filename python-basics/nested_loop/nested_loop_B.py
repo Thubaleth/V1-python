29b48fbe501e76d78f2ccfc58b641c8c7fa232ff
Thubalethu Njapha <thubzthubz@gmail.com>
2026-01-30 14:05:54 +0200

    nested loop practice

--- python-basics/nested_loop/nested_loop_B.py
+++ python-basics/nested_loop/nested_loop_B.py
@@ -26,3 +26,19 @@ def make_matrix(m, n, value):
     return matrix
     
 print(make_matrix(3, 5, None))
+#==========================================================
+# Write a function `total_product(matrix)` that returns the product of all numbers in a 2D list.
+
+def total_product(matrix):
+    product = 1
+    for row in matrix:
+        for num in row:
+            product *= num
+    return product
+
+array1 = [[3, 5, 2], [6, 2]]
+array2 = [[4, 6], [2, 3], [1, 2]]
+
+print(total_product(array1))  # 360
+print(total_product(array2))  # 288
+
